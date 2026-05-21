6115b9a47065a9dc45bfff6118a3109eeb8ecdf7
Huy Nguyen <nghuy164@gmail.com>
2026-05-22 02:31:46 +0300

    update

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,7 +1,7 @@
 import { useSelector } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 
-import { Searchbar, Sidebar, MusicPlayer, TopPlay } from '../renderer/components';
+import { Searchbar, Sidebar, MusicPlayer, TopPlay } from './components';
 import { ArtistDetails, TopArtists, AroundYou, Discover, Search, SongDetails, TopCharts } from './pages';
 
 function App() {
