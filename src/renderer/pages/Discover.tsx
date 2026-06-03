d9e5e04d84bcb4538306f72efc826bb8b8ecc447
Huy Nguyen <nghuy164@gmail.com>
2026-06-03 18:00:11 +0300

    update

--- src/renderer/pages/Discover.tsx
+++ src/renderer/pages/Discover.tsx
@@ -6,8 +6,8 @@ const Discover = () => {
   const genreTitle = 'Pop'
   return (
   <div className="flex flex-col">
-    <div className='w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10'>
-      <h2 className='font-bold text-3xl text-white text-left'>Discover {genreTitle}</h2>
+    <div className='w-full flex justify-center items-center sm:flex-row flex-col mt-4 mb-10'>
+      <h2 className='font-bold text-3xl text-white text-center'>Discover {genreTitle}</h2>
       <select
         onChange={() => {}}
         value=''
